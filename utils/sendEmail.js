cd07f09b2123ec7cb3c16d30d0c0e37f8e611c19
Yasin Nadaf <myasinnadaf6@gmail.com>
2025-12-23 20:16:50 +0530

    Switch email sending to SendGrid

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,19 +1,13 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
-
-  await transporter.sendMail({
-    from: `"ASAT Automation" <${process.env.EMAIL_USER}>`,
+  await sgMail.send({
     to,
+    from: process.env.EMAIL_FROM,
     subject,
-    html
+    html,
   });
 };
 
